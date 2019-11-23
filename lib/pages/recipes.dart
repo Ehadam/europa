c49032602b282fa1204905c5c9d7474641cae4aa
Ehadam <wastawonk@gmail.com>
2019-11-23 14:36:12 -0500

    Rename recipes

--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -1,12 +1,20 @@
-import 'package:europa/components/recipe_list.dart';
+import 'package:europa/components/recipe_card.dart';
 import 'package:europa/model/recipe.dart';
 import 'package:flutter/material.dart';
 
-class Recipies extends StatelessWidget {
+class Recipes extends StatelessWidget {
+
+  Recipes(this.recipes, this.addRecipe);
+
+  final List<Recipe> recipes;
+  final Function addRecipe;
+
   void _addRecipe(BuildContext context) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
+          // Need to dispose of this?
+          final controller = TextEditingController();
           return Dialog(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(5)),
@@ -21,7 +29,7 @@ class Recipies extends StatelessWidget {
                     'Recipe Name',
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                   ),
-                  TextField(),
+                  TextField(controller: controller,),
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
                     child: Row(
@@ -30,7 +38,10 @@ class Recipies extends StatelessWidget {
                         FlatButton(
                           child: Text('SAVE',
                               style: TextStyle(color: Colors.blue)),
-                          onPressed: () {},
+                          onPressed: () {
+                            addRecipe(Recipe(title: controller.text));
+                            Navigator.pop(context);
+                          },
                         ),
                       ],
                     ),
@@ -48,10 +59,10 @@ class Recipies extends StatelessWidget {
       appBar: AppBar(
         title: Text('Recipies!'),
       ),
-      body: RecipeList(<Recipe>[
-        Recipe(title: 'Test recipe'),
-        Recipe(title: 'Another recipe')
-      ]),
+      body: ListView.builder(
+        itemCount: recipes.length,
+        itemBuilder: (BuildContext context, int index) => RecipeCard(recipes[index])
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _addRecipe(context),
